68191872ea188656653c7a2d5fa25593447058b5
Arya Gupta <aryagupta239@gmail.com>
2025-10-11 23:55:48 +0530

    bird flies on touching space bar, screen is scrolling, pipes added not sure how to proceed next

--- main.py
+++ main.py
@@ -13,15 +13,34 @@ bg = pygame.image.load('./images/sky.jpg').convert()
 scroll = 0 
 tiles = math.ceil(1280  /bg.get_width()) + 1 
 
-player_pos = pygame.Vector2(screen.get_width() / 2 - 500, screen.get_height() / 2)
+bird = pygame.image.load("./images/bird.png")
+bird = pygame.transform.scale(bird, (50, 50))
+bird_speed = 8
+bird_x = screen.get_width() / 2 - 400
+bird_y = screen.get_height() / 2
+
+pipe = pygame.image.load('./images/pipe.png')
+pipe = pygame.transform.scale(pipe, (350, 350))
+pipe_x = screen.get_width()
+pipe_y = screen.get_height() 
+
+pipe_ulta = pygame.image.load('./images/pipe-ulta.png')
+pipe_ulta = pygame.transform.scale(pipe, (350, 350))
+# pipe_x = screen.get_width()
+# pipe_y = screen.get_height() 
+
 
 while running:
 
     clock.tick(50)
 
+    if bird_y > screen.get_height() : running = False
+
     i = 0
     while(i<tiles):
         screen.blit(bg, (bg.get_width()*i + scroll, 0))
+        screen.blit(pipe, (pipe.get_width()*i + scroll, 0))
+        screen.blit(pipe_ulta, (pipe_ulta.get_width()*i + scroll, screen.get_height() + 40))
         i+=1
     
     scroll -= 6
@@ -33,17 +52,15 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    pygame.draw.circle(screen, "black", player_pos, 20)
+        
 
     keys = pygame.key.get_pressed()
-    if keys[pygame.K_w]:
-        player_pos.y -= 300 * dt
-    if keys[pygame.K_s]:
-        player_pos.y += 300 * dt
-    if keys[pygame.K_a]:
-        player_pos.x -= 300 * dt
-    if keys[pygame.K_d]:
-        player_pos.x += 300 * dt
+    if keys[pygame.K_SPACE]: 
+        bird_y -= bird_speed * 2
+    bird_y += bird_speed
+
+    screen.blit(bird, (bird_x, bird_y))
+    
 
     pygame.display.flip()
     dt = clock.tick(60) / 1000
