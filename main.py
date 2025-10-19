9699ab017bea53b1aa4ffb5b08f580cb232139cc
Arya Gupta <aryagupta239@gmail.com>
2025-10-19 17:57:45 +0530

    mistake made as usual this code is useless

--- main.py
+++ main.py
@@ -1,85 +1,75 @@
 import pygame
-import random
+import math
 
-# Initialize setup
+# pygame setup
 pygame.init()
 screen = pygame.display.set_mode((1280, 720))
 clock = pygame.time.Clock()
+running = True
+dt = 0
 
-# Load assets
 bg = pygame.image.load('./images/sky.jpg').convert()
+
+scroll = 0 
+tiles = math.ceil(1280  /bg.get_width()) + 1 
+
 bird = pygame.image.load("./images/bird.png")
 bird = pygame.transform.scale(bird, (50, 50))
 bird_speed = 8
-bird_x = 200
-bird_y = screen.get_height() // 2
+bird_x = screen.get_width() / 2 - 400
+bird_y = screen.get_height() / 2
 
-pipe_img = pygame.image.load('./images/pipe.png')
-pipe_img = pygame.transform.scale(pipe_img, (180, 600))  # thicker width
-pipe_ulta_img = pygame.image.load('./images/pipe-ulta.png')
-pipe_ulta_img = pygame.transform.scale(pipe_ulta_img, (180, 600))  # thicker width
+pipe = pygame.image.load('./images/pipe.png')
+pipe = pygame.transform.scale(pipe, (350, 350))
+pipe_x = screen.get_width()
+pipe_y = screen.get_height() 
 
-# Pipe properties
-PIPE_WIDTH = 180
-PIPE_HEIGHT = 600
-GAP_HEIGHT = 100   # more space between top and bottom pipes
-PIPE_SPACING = 350
-SCROLL_SPEED = 4
+pipe_ulta = pygame.image.load('./images/pipe-ulta.png')
+pipe_ulta = pygame.transform.scale(pipe, (350, 350))
+# pipe_x = screen.get_width()
+# pipe_y = screen.get_height() 
 
-# Create pipes
-pipes = []
-for i in range(4):
-    x = 700 + i * PIPE_SPACING
-    offset = random.randint(200, 400)  # fixed offset range prevents floating pipes
-    pipes.append([x, offset])
 
-def reset_game():
-    """Resets bird and pipe positions."""
-    global bird_x, bird_y, pipes
-    bird_x = 200
-    bird_y = screen.get_height() // 2
-    pipes = []
-    for i in range(5):
-        x = 700 + i * PIPE_SPACING
-        offset = random.randint(200, 400)
-        pipes.append([x, offset])
-
-# Game loop
-running = True
 while running:
-    clock.tick(40)
-    screen.blit(bg, (0, 0))
+
+    clock.tick(50)
+
+    if bird_y > screen.get_height() : running = False
+
+    i = 0
+    while(i<tiles):
+        screen.blit(bg, (bg.get_width()*i + scroll, 0))
+        screen.blit(pipe, (pipe.get_width()*i + scroll, 0))
+        screen.blit(pipe_ulta, (pipe_ulta.get_width()*i + scroll, screen.get_height() + 40))
+        i+=1
+    
+    scroll -= 6
+
+    if abs(scroll) > bg.get_width():
+        scroll = 0
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
+        
+
     keys = pygame.key.get_pressed()
-    if keys[pygame.K_SPACE]:
+    if keys[pygame.K_SPACE]: 
         bird_y -= bird_speed * 2
     bird_y += bird_speed
 
-    # Scroll pipes
-    for pipe in pipes:
-        pipe_x, pipe_offset = pipe
-        # Top pipe (flipped)
-        screen.blit(pipe_ulta_img, (pipe_x, pipe_offset - PIPE_HEIGHT))
-        # Bottom pipe (normal)
-        screen.blit(pipe_img, (pipe_x, pipe_offset + GAP_HEIGHT))
-        pipe[0] -= SCROLL_SPEED
-
-        # Move off-screen pipes to right
-        if pipe[0] < -PIPE_WIDTH:
-            pipe[0] = screen.get_width()
-            pipe[1] = random.randint(200, 400)
-
-    # Draw bird
     screen.blit(bird, (bird_x, bird_y))
-
-    # Gameover condition
-    if bird_y > screen.get_height():
-        running = False
+    
 
     pygame.display.flip()
+    dt = clock.tick(60) / 1000
+    
+    pygame.display.update()
+    
+
 
 pygame.quit()
+
+
+ 
